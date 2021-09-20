dacc814ae3f552c6e13700f62d2c68a69e5a2706
Donevan Gonzales <donevan.lee.gonzales@gmail.com>
2021-09-20 15:43:30 -0500

    fixed accordion issue

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -18,14 +18,12 @@ const Accordion = ({ items }) => {
 
         return (
             <React.Fragment key={item.title}>
-                <div className={`title ${active}`} onClick={() => {onTitleClick(index)}}>
-                    <div>
-                        <i className="dropdown icon"></i>
-                        {item.title}
-                    </div>
-                    <div className={`content ${active}`}>
-                        <p>{item.content}</p>
-                    </div>
+                <div className={`${active} title`} onClick={() => {onTitleClick(index)}}>
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className={`content ${active}`}>
+                    <p>{item.content}</p>
                 </div>
             </React.Fragment>
         );
@@ -34,7 +32,6 @@ const Accordion = ({ items }) => {
     return (
         <div className="ui styled accordion">
             {renderedItems}
-            {activeIndex}
         </div>
     )
 }
